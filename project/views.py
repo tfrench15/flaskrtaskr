d93cf41410b6fd49c611ff4c026a98dffdd720b3
TIM FRENCH <tfrench15@gmail.com>
2017-12-09 17:35:08 -0800

    modify views

--- project/views.py
+++ project/views.py
@@ -1,7 +1,7 @@
 import sqlite3
 from functools import wraps
 
-from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask import Flask, flash, redirect, render_template, request, session, url_for, g 
 
 app = Flask(__name__)
 app.config.from_object('_config')
@@ -40,5 +40,24 @@ def login():
 			return redirect(url_for('tasks'))
 
 	return render_template('login.html')
+
+@app.route('/tasks')
+@login_required
+def tasks():
+	g.db = connect_db()
+	cursor = g.db.execute("SELECT name, due_date, priority, task_id FROM Tasks WHERE status = 0")
+	closed_tasks = [
+		dict(name = row[0], due_date = row[1], priority = row[2], task_id = row[3]) for row in cursor.fetchall()
+		]
+
+	g.db.close()
+	return render_template(
+		'tasks.html',
+		form = AddTaskForm(request.form),
+		open_tasks = open_tasks,
+		closed_tasks = closed_tasks
+		)
+
+
 
 
