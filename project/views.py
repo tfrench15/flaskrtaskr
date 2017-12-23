13d43dffbe87afd96023e47f935beecb322c52a2
TIM FRENCH <tfrench15@gmail.com>
2017-12-22 19:50:03 -0800

    switch datetime and status

--- project/views.py
+++ project/views.py
@@ -87,8 +87,8 @@ def create_task():
 				form.name.data,
 				form.due_date.data,
 				form.priority.data,
-				datetime.datetime.utcnow(),
-				'1', 
+				'1',
+				datetime.datetime.utcnow(), 
 				session['user_id'], 
 			)
 			db.session.add(new_task)
