64665ac6715c2e9c25966bd63d06ca247c60862e
TIM FRENCH <tfrench15@gmail.com>
2017-12-22 18:21:56 -0800

    second POST fix

--- project/forms.py
+++ project/forms.py
@@ -1,13 +1,13 @@
-from flask_wtf import FlaskForm 
+from flask_wtf import Form 
 from wtforms import StringField, DateField, IntegerField, SelectField, PasswordField
 from wtforms.validators import DataRequired, Length, EqualTo
 
-class AddTaskForm(FlaskForm):
+class AddTaskForm(Form):
 	task_id = IntegerField()
 	name = StringField('Task Name', validators = [DataRequired()])
 	due_date = DateField(
 		'Date Due (mm/dd/yyyy)',
-		validators = [DataRequired()], format = '%m/%d/%y'
+		validators = [DataRequired()], format = '%m/%d/%Y'
 	)
 	priority = SelectField(
 		'Priority',
