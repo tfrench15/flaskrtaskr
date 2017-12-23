94c990a842bef092041f6f2ce39e8b3c3cf71504
TIM FRENCH <tfrench15@gmail.com>
2017-12-23 00:03:48 -0800

    require email import

--- project/forms.py
+++ project/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import Form 
 from wtforms import StringField, DateField, IntegerField, SelectField, PasswordField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 class AddTaskForm(Form):
 	task_id = IntegerField()
