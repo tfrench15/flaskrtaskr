3894bd08ac3a6d0d46269d1f62a1cb1314b7454b
TIM FRENCH <tfrench15@gmail.com>
2017-12-27 13:31:45 -0600

    final test

--- project/test.py
+++ project/test.py
@@ -149,6 +149,18 @@ class AllTests(unittest.TestCase):
 		self.create_task()
 		response = self.app.get("delete/1/", follow_redirects = True)
 		self.assertIn(b'The task was deleted', response.data)
+
+	def test_users_cannot_complete_tasks_that_are_not_created_by_them(self):
+		self.create_user('Amanda', 'amandabfrench@gmail.com', 'camelback')
+		self.login('Amanda', 'camelback')
+		self.app.get('tasks/', follow_redirects = True)
+		self.create_task()
+		self.logout()
+		self.create_user('Jenn', 'jennfrench@gmail.com', 'hoboken')
+		self.login('Jenn', 'hoboken')
+		self.app.get('tasks/', follow_redirects = True)
+		response = self.app.get("complete/1/", follow_redirects = True)
+		self.assertNotIn(b'The task is complete', response.data)
 		
 
 
