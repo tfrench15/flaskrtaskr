0b4bfbd459a3f4492c2ebcb1d9e78e9d9d7b01b8
TIM FRENCH <tfrench15@gmail.com>
2017-12-28 11:20:57 -0600

    eliminate test_helpers module

--- project/test_users.py
+++ project/test_users.py
@@ -11,6 +11,61 @@ TEST_DB = 'test.db'
 
 class TestUsers(unittest.TestCase):
 
+	#Helper Methods
+
+	def setUp(self):
+		app.config['TESTING'] = True
+		app.config['WTF_CSRF_ENABLED'] = False
+		app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, TEST_DB)
+		self.app = app.test_client()
+		db.create_all()
+
+	def tearDown(self):
+		db.session.remove()
+		db.drop_all()
+
+	def login(self, name, password):
+		return self.app.post('/', data = dict(name = name, password = password), follow_redirects = True)
+
+	def register(self, name, email, password, confirm):
+		return self.app.post(
+			'register/',
+			data = dict(name = name, email = email, password = password, confirm = confirm),
+			follow_redirects = True
+		)
+
+	def logout(self):
+		return self.app.get('logout/', follow_redirects = True)
+
+	def create_user(self, name, email, password):
+		new_user = User(name = name, email = email, password = password)
+		db.session.add(new_user)
+		db.session.commit()
+
+	def create_admin_user(self, name, email, password, role):
+		new_user = User(
+			name = 'Superman', 
+			email = 'admin@flaskrtaskr.com',
+			password = 'allpowerful',
+			role = 'admin'
+		)
+		db.session.add(new_user)
+		db.session.commit()
+
+
+	def create_task(self):
+		return self.app.post('add/', data = dict(
+			name = 'Go to the bank',
+			due_date = '01/01/2018',
+			priority = '1',
+			posted_date = '12/24/2017',
+			status = '1'
+			), follow_redirects = True
+		)
+
+
+	#Testing Methods
+
 	def test_user_setup(self):
 		new_user = User("tim", "tim@tpfrench.com", "timfrench")
 		db.session.add(new_user)
