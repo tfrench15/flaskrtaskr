c8b7961bed09ace77230d7eddea8259afc16e438
TIM FRENCH <tfrench15@gmail.com>
2017-12-28 21:26:34 -0600

    fix init

--- project/__init__.py
+++ project/__init__.py
@@ -7,7 +7,7 @@ app.config.from_pyfile('_config.py')
 db = SQLAlchemy(app)
 
 from project.users.views import users_blueprint
-from project.users.tasks import tasks_blueprint
+from project.tasks.views import tasks_blueprint
 
 app.register_blueprint(users_blueprint)
 app.register_blueprint(tasks_blueprint)
