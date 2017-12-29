4b5cc2fca7fed4231b0b2f8794fd3b6328645c04
TIM FRENCH <tfrench15@gmail.com>
2017-12-28 21:24:51 -0600

    modify forms

--- project/forms.py
+++ project/forms.py
@@ -2,6 +2,8 @@ from flask_wtf import Form
 from wtforms import StringField, DateField, IntegerField, SelectField, PasswordField
 from wtforms.validators import DataRequired, Length, EqualTo, Email
 
+
+
 class AddTaskForm(Form):
 	task_id = IntegerField()
 	name = StringField('Task Name', validators = [DataRequired()])
