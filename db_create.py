5958ea9c2323f6cee249481af0edafe31516261c
TIM FRENCH <tfrench15@gmail.com>
2017-12-28 21:34:34 -0600

    create new db

--- db_create.py
+++ db_create.py
@@ -1,12 +1,19 @@
-
-from views import db
-from models import Task
-from models import User
 from datetime import date
 
+from project import db
+from project.models import Task, User
+
+
 db.create_all()
-'''
-db.session.add(Task("Finish this tutorial", date(2016, 12, 15), 10, 1))
-db.session.add(Task("Finish Real Python", date(2017, 12, 31), 10, 1))
-'''
+
+db.session.add(
+	User("admin, ad@min.com", "admin", "admin")
+)
+db.session.add(
+	Task("Finish this tutorial", date(2017, 12, 31), 10, date(2017, 12, 31), 1, 1)
+)
+db.session.add(
+	Task("Finish RealPython", date(2018, 01, 31), 10, date(2018, 01, 31), 1, 1)
+)
 db.session.commit()
+
