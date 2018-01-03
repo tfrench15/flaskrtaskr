cbbd9977171f7d16d642474de00411652ddb49ff
TIM FRENCH <tfrench15@gmail.com>
2018-01-03 00:08:22 -0800

    migrate db

--- db_create.py
+++ db_create.py
@@ -6,14 +6,6 @@ from project.models import Task, User
 
 db.create_all()
 
-db.session.add(
-	User("admin, ad@min.com", "admin", "admin")
-)
-db.session.add(
-	Task("Finish this tutorial", date(2017, 12, 31), 10, 1, 1, date(2018, 1, 31))
-)
-db.session.add(
-	Task("Finish RealPython", date(2018, 1, 31), 10, 1, 1, date(2018, 1, 31))
-)
+
 db.session.commit()
 
