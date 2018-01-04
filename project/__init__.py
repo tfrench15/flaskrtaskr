d7fae57058314755f8b87e476d6e37072e8f3e3c
TIM FRENCH <tfrench15@gmail.com>
2018-01-04 12:35:05 -0800

    init 404

--- project/__init__.py
+++ project/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy 
 from flask_bcrypt import Bcrypt 
 
@@ -13,3 +13,7 @@ from project.tasks.views import tasks_blueprint
 
 app.register_blueprint(users_blueprint)
 app.register_blueprint(tasks_blueprint)
+
+@app.errorhandler(404)
+def not_found(error):
+	return render_template('404.html'), 404
