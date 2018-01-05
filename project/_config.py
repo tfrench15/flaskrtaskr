572ab6dac37cc9d0a2151e274aaf41845f82a6d4
TIM FRENCH <tfrench15@gmail.com>
2018-01-04 20:33:53 -0800

    prepping deploy

--- project/_config.py
+++ project/_config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DATABASE = 'flasktaskr.db'
 WTF_CSRF_ENABLED = True
 SECRET_KEY = '\x1ev\xb4\x13\xb3TQl1\x8c\x10 \x00\xc7\x9f\x02\xb3\x8a\xd9}\\\xc9\x088'
-DEBUG = True
+DEBUG = False
 
 DATABASE_PATH = os.path.join(basedir, DATABASE)
 
