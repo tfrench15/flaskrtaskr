318ff0e4feff9d19003c698c251677f255f78236
TIM FRENCH <tfrench15@gmail.com>
2018-01-05 18:05:19 -0800

    api views

---
+++ project/api/views.py
@@ -0,0 +1,48 @@
+
+from functools import wraps
+from flask import flash, redirect, jsonify, url_for, session, Blueprint
+
+from project import db
+from project.models import Task
+
+api_blueprint = Blueprint('api', __name__)
+
+def login_required(test):
+	@wraps(test)
+	def wrap(*args, **kwargs):
+		if 'logged_in' in session:
+			return test(*args, **kwargs)
+		else:
+			flash('You need to log in first')
+			return redirect(url_for('users.login'))
+	return wrap
+
+
+def open_tasks():
+	return db.session.query(Task).filter_by(status = '1').order_by(Task.due_date.asc())
+
+
+def closed_tasks():
+	return db.session.query(Task).filter_by(status = '0').order_by(Task.due_date.asc())
+
+
+@api_blueprint.route('/api/v1/tasks/')
+def api_tasks():
+	results = db.session.query(Task).limit(10).offset(0).all()
+	json_results = []
+	for result in results:
+		data = {
+			'task_id': result.task_id,
+			'task name': result.name,
+			'due date': str(result.due_date),
+			'priority': result.priority,
+			'posted date': str(result.posted_date),
+			'status': result.status,
+			'user id': result.user_id
+			}
+
+		json_results.append(data)
+
+	return jsonify(items = json_results)
+
+	
