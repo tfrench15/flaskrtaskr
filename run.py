5fb889c62b1a199ec6223a77a513930d1928dbc4
TIM FRENCH <tfrench15@gmail.com>
2018-01-05 19:07:44 -0800

    revert run

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 
-#import os
+import os
 from project import app
 
-#port = int(os.environ.get('PORT', 5000))
-#app.run(host = '0.0.0.0', port = port)
+port = int(os.environ.get('PORT', 5000))
+app.run(host = '0.0.0.0', port = port)
 
-app.run()
