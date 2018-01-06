dd105a5c6c76b727b441a019d2a6692952b9dc53
TIM FRENCH <tfrench15@gmail.com>
2018-01-05 17:51:38 -0800

    add api tests

---
+++ tests/test_api.py
@@ -0,0 +1,64 @@
+
+import os
+import unittest
+from datetime import data
+
+from project import app, db
+from project._config import basedir
+from project.models import Task
+
+
+TEST_DB = 'test.db'
+
+class APITests(unittest.TestCase):
+
+	def setUp(self):
+		app.config['TESTING'] = True
+		app.config['WTF_CSRF_ENABLED'] = False
+		app.config['DEBUG'] = False
+		app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, TEST_DB)
+		self.app = app.test_client()
+		db.create_all()
+
+		self.assertEquals(app.debug, False)
+
+	def tearDown(self):
+		db.session.remove()
+		db.drop_all()
+
+	def add_tasks(self):
+		db.session.add(
+			Task(
+				"Run around in circles",
+				date(2018, 1, 10),
+				10,
+				1,
+				date(2018, 1, 12)
+			)
+		)
+		db.session.commit()
+
+		db.session.add(
+			Task(
+				"Purchase Real Python",
+				date(2018, 1, 15),
+				10,
+				1,
+				date(2018, 1, 17)
+			)
+		)
+		db.session.commit()
+
+	def test_collection_endpoint_returns_correct_data(self):
+		self.add_tasks()
+		response = self.app.get('api/v1/tasks/', follow_redirects = True)
+		self.assertEquals(response.status_code, 200)
+		self.assertEquals(response.mimetype, 'application/json')
+		self.assertIn(b'Run around in circles', response.data)
+		self.assertEquals(b'Purchase Real Python', response.data)
+
+
+if __name__ == "__main__":
+	unittest.main()
+
+
